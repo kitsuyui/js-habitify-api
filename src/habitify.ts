cee52d499577d45e0d7cf7f5eba71b7bd25feef3
kitsuyui <kitsuyui@kitsuyui.com>
2023-07-25 13:45:41 +0900

    Add doc comment

--- src/habitify.ts
+++ src/habitify.ts
@@ -6,9 +6,19 @@ import {
   LogsAPIResult,
 } from './types'
 
+/**
+ * Client for Habitify API.
+ * @param apiKey {string}
+ * @returns Client
+ */
 export class Client {
   constructor(private apiKey: string) {}
 
+  /**
+   * Get Client instance from environment variables (HABITIFY_API_KEY)
+   * instead of passing the key directly.
+   * @returns Client
+   */
   static getClientFromEnv(): Client {
     const apiKey = process.env.HABITIFY_API_KEY
     if (apiKey === undefined) {
@@ -17,6 +27,12 @@ export class Client {
     return new Client(apiKey)
   }
 
+  /**
+   * Fetch data from Habitify API.
+   * @param url
+   * @returns data from Habitify API {T}
+   * @throws Error if fetch failed
+   */
   async fetch<T>(url: string): Promise<T> {
     const response = await fetch(url, {
       headers: {
@@ -31,6 +47,11 @@ export class Client {
     return data
   }
 
+  /**
+   * Fetch journal data from Habitify API.
+   * @param targetDate A subset of ISO 8601 format: https://docs.habitify.me/date-format (e.g. 2023-07-24T00:00:00+09:00) {DateString}
+   * @returns data from Habitify API {JournalAPIResult}
+   */
   async fetchJournal(targetDate: DateString): Promise<JournalAPIResult> {
     const query = new URLSearchParams({
       target_date: targetDate,
@@ -39,6 +60,14 @@ export class Client {
     return await this.fetch<JournalAPIResult>(url)
   }
 
+  /**
+   * Fetch logs data from Habitify API.
+   * @param habitId {string}
+   * @param fromDate A subset of ISO 8601 format: https://docs.habitify.me/date-format (e.g. 2023-07-01T00:00:00+09:00) {DateString}
+   * @param toDate A subset of ISO 8601 format: https://docs.habitify.me/date-format (e.g. 2023-07-24T00:00:00+09:00) {DateString}
+   * @throws Error if fetch failed
+   * @returns data from Habitify API {LogsAPIResult}
+   */
   async fetchLogs({
     habitId,
     fromDate,
