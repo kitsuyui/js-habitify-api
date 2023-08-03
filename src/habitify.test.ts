b118f5f8ac009525b392b4d1a2caf94520a646a1
kitsuyui <kitsuyui@kitsuyui.com>
2023-08-03 22:53:50 +0900

    Enhance test coverage
    - Test for getClientFromEnv
    - Test for fetch failed

--- src/habitify.test.ts
+++ src/habitify.test.ts
@@ -3,6 +3,26 @@ import { describe, it, expect } from '@jest/globals'
 import { Client } from './habitify'
 
 describe('habitify client', () => {
+  it('getClientFromEnv', () => {
+    const client = Client.getClientFromEnv()
+    expect(client).toBeDefined()
+  })
+
+  it('getClientFromEnv failed', () => {
+    const oldEnv = process.env
+    process.env = {}
+    expect(() => Client.getClientFromEnv()).toThrowError(
+      'HABITIFY_API_KEY is not set'
+    )
+    process.env = oldEnv
+  })
+
+  it('failed to fetch for invalid URL', async () => {
+    const client = Client.getClientFromEnv()
+    const result = client.fetch('https://api.habitify.me/v0/')
+    await expect(result).rejects.toThrowError('fetch failed: Not Found')
+  })
+
   it('fetchJournal', async () => {
     const client = Client.getClientFromEnv()
     const result = await client.fetchJournal('2023-07-24T00:00:00+09:00')
